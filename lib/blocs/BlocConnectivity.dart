4d6c0240ac8f244cae3fea1be7f6345112a92e8e
Jocaagura <oficinameltablero@gmail.com>
2022-11-01 20:47:23 -0500

    starting model of connectivity

---
+++ lib/blocs/BlocConnectivity.dart
@@ -0,0 +1,6 @@
+import '../entities/entity_bloc.dart';
+
+class BlocConnectivity extends EntityBloc {
+  @override
+  void dispose() {}
+}
---
+++ lib/entities/entity_model.dart
@@ -0,0 +1,4 @@
+abstract class EntityModel {
+  // returns a JSON style model from entity Model
+  Map<String, dynamic> toJson();
+}
